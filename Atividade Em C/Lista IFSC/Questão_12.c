3e0bb680902684f2d2de94229764ec03feb0d2cb
Lucas Regis <workslucas25@gmail.com>
2025-04-22 20:15:16 -0300

    Create Questão_12.c

---
+++ Atividade Em C/Lista IFSC/Questão_12.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+
+int main() { 
+
+  float c,f; 
+
+   printf("Informe a temperatura em Graus Celsius: "); 
+   scanf("%f",&c); 
+   f=(9*c+160)/5; 
+   printf("Sua temperatura em graus fahrenheit: %.1f ",f); 
+
+    return 0; 
+
+} 
