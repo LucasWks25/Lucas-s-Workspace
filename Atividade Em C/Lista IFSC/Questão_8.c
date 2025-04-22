8e49b5dd292fd524cf2c11397160b8473e340047
Lucas Regis <workslucas25@gmail.com>
2025-04-22 20:11:04 -0300

    Create Questão_8.c

---
+++ Atividade Em C/Lista IFSC/Questão_8.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+
+int main() { 
+
+   float tri,alt,area; 
+   printf("Informe a base do triangulo "); 
+   scanf("%f",&tri); 
+   printf("Informe a altura do triangulo "); 
+   scanf("%f",&alt); 
+   area=tri*alt/2; 
+   printf("A area da circunferencia é %.2f",area); 
+    return 0; 
+
+} 
