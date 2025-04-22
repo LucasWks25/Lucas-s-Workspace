647ea835582dd5c6d5a69c3b475fef0dd8e13f0d
Lucas Regis <workslucas25@gmail.com>
2025-04-22 20:25:05 -0300

    Create Questão_18.c

---
+++ Atividade Em C/Lista IFSC/Questão_18.c
@@ -0,0 +1,19 @@
+#include <stdio.h>  
+
+int main() { 
+
+float comp,alt,larg; 
+
+printf("Informe o comprimento : ");  
+scanf("%f",&comp);  
+printf("Informe a altura : "); 
+scanf("%f",&alt);  
+printf("Informe a largura : "); 
+scanf("%f",&larg);  
+printf("o Volume do objeto retangular é: %.2f m³",+comp*alt*larg);  
+
+return 0;  
+
+} 
+
+ 
