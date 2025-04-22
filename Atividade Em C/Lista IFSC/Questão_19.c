64e96781bcbc2db8b97c96497c937d217bc46678
Lucas Regis <workslucas25@gmail.com>
2025-04-22 20:25:42 -0300

    Create Questão_19.c

---
+++ Atividade Em C/Lista IFSC/Questão_19.c
@@ -0,0 +1,13 @@
+#include <stdio.h>  
+
+int main() { 
+
+int n; 
+
+printf("Informe um numero : ");  
+scanf("%i",&n); 
+printf("o Valor do seu numero elevado ao quadrado é: %i\n ",+n*n); 
+printf("o Valor do seu numero elevado ao cubo é: %i ",+n*n*n);  
+
+return 0;  
+} 
