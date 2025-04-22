a7e1a5b17fd7bcf723fa25567c837481018e2b08
Lucas Regis <workslucas25@gmail.com>
2025-04-22 20:37:53 -0300

    Create Questão_22.c

---
+++ Atividade Em C/Lista IFSC/Questão_22.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+ int main() {  
+ float Val1,Val2,media;  
+ printf("Informe um numero : "); 
+ scanf("%f",&Val1);
+ printf("Informe um numero : "); 
+ scanf("%f",&Val2);
+ media=(Val1+Val2)/2;
+ printf("A Media dos seus numeros é: %.2f\n ", media); 
+return 0;
+}
