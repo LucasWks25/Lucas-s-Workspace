5474f76101de5fea404731f4cb5403df59414d48
Lucas Regis <workslucas25@gmail.com>
2025-04-22 20:09:49 -0300

    Create Questão_7.c

---
+++ Atividade Em C/Lista IFSC/Questão_7.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+
+int main() {  
+float raio,pi,area;  
+pi=3.14;  
+printf("Informe o raio ");  
+scanf("%f",&raio); 
+area=pi*(raio*raio);  
+printf("A area da circunferencia é %.2f",area); 
+
+return 0;
+}
