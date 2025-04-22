aafdc80d8d78c52918aecf74733c4d31539118dd
Lucas Regis <workslucas25@gmail.com>
2025-04-22 20:48:35 -0300

    Update and rename Questão_22 to Questão_24.c

---
+++ Atividade Em C/Lista IFSC/Questão_24.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+
+ int main() { 
+   
+ float a,b;  
+   
+ printf("Informe os Km/h  : "); 
+ scanf("%f",&a);
+ b=a/3.6;
+ printf("Sua velocidade em m/s é %.2f ",b); 
+return 0;
+}
