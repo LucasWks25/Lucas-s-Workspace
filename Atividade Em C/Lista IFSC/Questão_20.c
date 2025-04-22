d518ac76b931c63f8ad8cc3bd2165643376923e1
Lucas Regis <workslucas25@gmail.com>
2025-04-22 20:26:19 -0300

    Create Questão_20.c

---
+++ Atividade Em C/Lista IFSC/Questão_20.c
@@ -0,0 +1,14 @@
+#include <stdio.h>  
+ 
+int main() {   
+
+int a,b;   
+
+printf("Informe um numero : ");  
+scanf("%i",&a); 
+printf("Informe um numero : ");  
+scanf("%i",&b); 
+printf("o Valor do quadrado da soma de seus numeros é: %i\n ",+(a+b)*(a+b));  
+return 0; 
+
+} 
