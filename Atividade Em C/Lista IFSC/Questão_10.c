2afd52d839cd6b3acb5fcea4cdcc8ab4d8f942df
Lucas Regis <workslucas25@gmail.com>
2025-04-22 20:12:49 -0300

    Create Questão_10.c

---
+++ Atividade Em C/Lista IFSC/Questão_10.c
@@ -0,0 +1,15 @@
+#include <stdio.h>  
+
+int main() { 
+
+   int x,y,result; 
+   printf("Informe o primeiro numero: "); 
+   scanf("%i",&x); 
+   printf("Informe o segundo numero: "); 
+   scanf("%i",&y); 
+   result= x+y; 
+   printf("O resultado da sua soma é: %i",result); 
+
+    return 0; 
+
+} 
