1c8677e9157675d2d7341b0c1591704082718003
Lucas Regis <workslucas25@gmail.com>
2025-04-22 20:41:49 -0300

    Update Questão_23.c

--- Atividade Em C/Lista IFSC/Questão_23.c
+++ Atividade Em C/Lista IFSC/Questão_23.c
@@ -1,1 +1,14 @@
 
+#include <stdio.h>
+
+ int main() { 
+   
+ int a,b;  
+ printf("Informe A  : "); 
+ scanf("%i",&a);
+ printf("Informe B : "); 
+ scanf("%i",&b);
+
+ printf("A é %d e B é %d ",b,a); 
+return 0;
+}
