8561a35cb2fe93898b195a1841aeaacab43d14ce
Lucas Regis <workslucas25@gmail.com>
2025-04-22 20:52:05 -0300

    Create Questão_25.c

---
+++ Atividade Em C/Lista IFSC/Questão_25.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+ int main() {  
+   
+ int int1,int2;  
+   
+ printf("Informe o Primeiro numero : "); 
+ scanf("%i",&int1);
+ printf("Informe o Segundo numero : "); 
+ scanf("%i",&int2);
+ printf("Seu quoeficiente é  %i e o resto é %d ",+(int1/int2),(int1%int2)); 
+return 0;
+}
