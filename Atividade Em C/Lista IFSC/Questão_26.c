cab3e34e04da09c38018df6fdf5abbad667d3b7d
Lucas Regis <workslucas25@gmail.com>
2025-04-22 21:01:39 -0300

    Create Questão_26.c

---
+++ Atividade Em C/Lista IFSC/Questão_26.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+
+ int main() {  
+   
+ float sal,salb,salc;  
+printf("Informe o seu salario : "); 
+scanf("%f",&sal);
+salb=0.10*sal;
+salc=sal-salb;
+printf("Seu Salario após o INSS é %.2f e após os impostos é %.2f",salc,salc-(salc*0.05));
+
+return 0;
+}
