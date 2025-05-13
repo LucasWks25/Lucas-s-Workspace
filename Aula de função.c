e986ff1f62b4fd29aa85a96e82696305bfafb398
Lucas Regis <workslucas25@gmail.com>
2025-05-13 20:57:35 -0300

    Update Aula de função.c

--- Aula de função.c
+++ Aula de função.c
@@ -1,15 +1,1 @@
-// Online C compiler to run C program online
-#include <stdio.h>
-void quadcub(int num){
-    printf("O Quadrado de %d é %d \n ",num , num*num);
-    printf("O Cubo de %d é %d \n ",num , num*num*num);
-}
 
-int main() {
-    int num;
-    // Write C code here
-    printf("informe o numero: \n");
-    scanf("%d",&num);
-    quadcub(num);
-    return 0;
-}
