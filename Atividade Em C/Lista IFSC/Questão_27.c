44c5bff858490f92c53e8268438439acaee3b56a
Lucas Regis <workslucas25@gmail.com>
2025-05-13 14:42:35 -0300

    Update Questão_27.c

--- Atividade Em C/Lista IFSC/Questão_27.c
+++ Atividade Em C/Lista IFSC/Questão_27.c
@@ -1,1 +1,25 @@
+#include <stdio.h> 
 
+ int main() {
+  
+ int a,b,c,d,e;
+ int p;
+ int s;
+ 
+
+ printf("Informe de a : "); 
+ scanf("%i",&a);
+ printf("Informe de b : "); 
+ scanf("%i",&b);
+ printf("Informe de c : "); 
+ scanf("%i",&c);
+ printf("Informe de d : "); 
+ scanf("%i",&d);
+ printf("Informe de e : "); 
+ scanf("%i",&e);
+ 
+ s=6*a+5*b+4*c+3*d+2*e;
+ p=s%7;
+ printf("Seu digito verificador é %d ",p); 
+ return 0;
+}
