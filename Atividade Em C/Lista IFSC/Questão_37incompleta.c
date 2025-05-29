de996b8641de5670ebc1fd811f70533c3040eb2a
Lucas Regis <workslucas25@gmail.com>
2025-05-29 00:35:16 -0300

    Update Questão_37incompleta.c

--- Atividade Em C/Lista IFSC/Questão_37incompleta.c
+++ Atividade Em C/Lista IFSC/Questão_37incompleta.c
@@ -1,18 +1,11 @@
+// Online C compiler to run C program online
 #include <stdio.h>
-#include <string.h>
 
 int main() {
-    int dia,mes,ano,y,diaat,mesat,anoat,temp1,temp2,temp3;
-    printf("Informe o dia mes e ano escrevendo ddmmaa: ");
-    scanf("%d %d %d",&dia,&mes,&ano);
-   printf("Informa o dia mês e o ano atual escrevendo ddmmaa: ")
-   scanf("%d %d %d",&diaat,&mesat,&anoat);
-   temp1=anoat-ano;
-   temp2=mesat-mes;
-   temp3=temp1*365
-   temp1=temp2*30
-   
-   
-    
+int dias,anos;
+    printf("Informe quantos anos você tem: ");
+    scanf("%d",&anos);
+    dias=anos*360;
+    printf("Você viveu %d dias", dias);
     return 0;
 }
