f5f70fa1d0557d2ef53bedef66444c63f87e75c3
Lucas Regis <workslucas25@gmail.com>
2025-05-29 12:22:26 -0300

    Create Questão_42.c

---
+++ Atividade Em C/Lista IFSC/Questão_42.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    char produto[100];
+    float desc,preco,precodesc,precotot,descp;
+    printf("Informe o nome do produto: ");
+    fgets(produto,sizeof(produto),stdin);
+    printf("Informe o preço do produto: ");
+    scanf("%f",&preco);
+    printf("Informe o desconto no produto: ");
+    scanf("%f",&desc);
+    descp=desc/100;
+    precodesc=preco*descp;
+    precotot=preco-precodesc;
+    printf("Nome do produto: %s\n",produto);
+    printf("Preço do produto: %.2f\n",preco);
+    printf("Preço do desconto: %.2f\n",precodesc);
+    printf("preço do produto com desconto: %.2f\n",precotot);
+}
