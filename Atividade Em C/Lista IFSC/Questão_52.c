f2b7a7404bd631e1544db5a73d2d8606b7daa393
Lucas Regis <workslucas25@gmail.com>
2025-05-30 19:24:51 -0300

    Create Questão_52.c

---
+++ Atividade Em C/Lista IFSC/Questão_52.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    printf("Informe A:\n");
+    scanf("%d",&a);
+    printf("Informe B:\n");
+    scanf("%d",&b);
+    if(a==b){
+        printf("os numeros são iguais")
+    }
+    if(a!=b){
+        printf("os numeros são Diferentes")
+    }
+    }if(a=>b){
+        printf("O Maior número é:%d",a);
+        printf("O Menor número é:%d",b);
+    }
+    if(a<=b){
+        printf("O Maior número é:%d",b);
+        printf("O Menor número é:%d",a);
+    }
+    }
