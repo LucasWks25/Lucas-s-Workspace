0b79746afea5dad1d70cfa33c285913f6182b568
Lucas Regis <workslucas25@gmail.com>
2025-05-30 19:11:15 -0300

    Create Questão_51.c

---
+++ Atividade Em C/Lista IFSC/Questão_51.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    printf("Informe A:\n");
+    scanf("%d",&a);
+    printf("Informe B:\n");
+    scanf("%d",&b);
+    if(a>b){
+        printf("O Maior número é:%d",a);
+    }
+    if(a<b){
+        printf("O Maior número é:%d",b);
+    }
+    }
