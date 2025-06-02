5d9cef7862d0e15cf9353c8023e3620f0048f5dc
Lucas Regis <workslucas25@gmail.com>
2025-06-02 00:01:57 -0300

    Create Questão_70.cc

---
+++ Atividade Em C/Lista IFSC/Questão_70.cc
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+int num1;
+
+    printf("Informe o numero:\n");
+    scanf("%d", &num1);
+    
+    if(num1%2==0){
+        printf("Verdadeiro");
+    }
+    return 0;
+}
