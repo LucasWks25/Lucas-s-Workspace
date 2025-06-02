01801273f372736ca168cfda6c683e498b9d1fca
Lucas Regis <workslucas25@gmail.com>
2025-06-02 15:35:33 -0300

    Update Questão_1.c

--- Atividade Em C/Lista PUCRS/Questão_1.c
+++ Atividade Em C/Lista PUCRS/Questão_1.c
@@ -1,1 +1,17 @@
+// Online C compiler to run C program online
+#include <stdio.h>
 
+int main() {
+    int a[5],x;
+    x=0;
+    for(int i=0;i<5;i++){
+    printf("Informe o Valor do %dº número: ",+i+1);
+    scanf("%d",&a[i]);
+    if(a[i]<0){
+    x=x+1;
+    }
+    }
+    printf("%d são negativos",x);
+    
+    return 0;
+}
